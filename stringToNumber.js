607ef7525662bcd3e7dc31b80127df8e5d5427b7
Yomato <37809541+yconway@users.noreply.github.com>
2019-12-04 17:21:20 -0600

    Create stringToNumber.js

---
+++ stringToNumber.js
@@ -0,0 +1,20 @@
+value =  '10' + 1
+console.log("'10' + 1: " + value) //'10' + 1: 101
+
+value =  1 + '10'
+console.log("1 + '10': " + value) //1 + '10': 110
+
+value =  1 + +'10'
+console.log("1 + +'10': " + value) //1 + +'10': 11
+
+value =  1 - '10'
+console.log("1 - '10': " + value) //1 - '10': -9
+
+value =  '10' - 1
+console.log("'10' - 1: " + value) //'10' - 1: 9
+
+value =  +"10"
+console.log('+"10": '+ value) //+"10": 10
+
+value =  -"10"
+console.log('-"10": '+ value) //-"10": -10 
